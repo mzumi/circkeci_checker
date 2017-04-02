a01549774e774547731833476af94fe01084fd83
mzumi <masazumi.m@gmail.com>
2017-04-02 12:27:47 +0900

    apply rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,39 @@ use std::error::Error;
 
 fn main() {
     match print_projects() {
-        Ok(()) => return ,
+        Ok(()) => return,
         Err(ref error) => println!("{}", error.description()),
     }
 }
 
-fn print_projects() -> Result<(), circleci_checker::Error>{
+fn print_projects() -> Result<(), circleci_checker::Error> {
     let projects = circleci_checker::service::fetch_projects()?;
 
     println!("CircleCI");
     println!("---");
 
     for project in projects {
-        println!("{}/{} | href={}", project.username, project.reponame, project.vcs_url);
+        println!("{}/{} | href={}",
+                 project.username,
+                 project.reponame,
+                 project.vcs_url);
         for (branch_name, branch) in project.branches {
-            let url = format!("https://circleci.com/gh/{}/{}/tree/{}", project.username, project.reponame, branch_name);
+            let url = format!("https://circleci.com/gh/{}/{}/tree/{}",
+                              project.username,
+                              project.reponame,
+                              branch_name);
             let recent_build = match branch.recent_builds {
                 Some(ref builds) if builds.len() > 0 => &builds[0],
-                _ => continue
+                _ => continue,
             };
 
-            let status = circleci_checker::Status::get(recent_build.outcome.as_str()).expect("ビルド状態が未定義です");
-            println!("- {} {} | color={} href={}", status.symbol(), branch_name.replace("%2F", "/"), status.color(), url);
+            let status = circleci_checker::Status::get(recent_build.outcome.as_str())
+                .expect("ビルド状態が未定義です");
+            println!("- {} {} | color={} href={}",
+                     status.symbol(),
+                     branch_name.replace("%2F", "/"),
+                     status.color(),
+                     url);
         }
         println!("---")
     }
